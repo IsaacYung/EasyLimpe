b1c5fc4b4bc52fbeeb03c3b9e49d1463a5120f86
IsaacYung <isaaczoi@gmail.com>
2016-03-25 00:53:16 -0300

    Start header

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'visitor#index'
+  root 'general#index'
+  # get 'geral/index', to: 'general#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
