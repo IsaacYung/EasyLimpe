d27853dcb5999d8f834bec8ef5852d730b8559bc
IsaacYung <isaaczoi@gmail.com>
2016-03-27 19:07:00 -0300

    Add register control

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'register/new'
+
+  get 'cadastro/novo', to: 'register#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
